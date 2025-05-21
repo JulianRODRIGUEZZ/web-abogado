bdd8565a5ff2f32ed9be99c4cd037e794f2cf837
JulianRODRIGUEZZ <julian.9911@hotmail.com>
2025-05-21 09:51:02 -0500

    serverless vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
-
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  
+
+  output: "server",
+  adapter: vercel(),
 });
